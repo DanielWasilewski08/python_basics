c4c08ebf811cb25db1e610c23873c07a114e54a2
DanielWasilewski08 <danielwasilewski08@gmail.com>
2025-03-15 20:29:14 +0000

    graph update - comments given

--- project2/machine_learning_graph/script.py
+++ project2/machine_learning_graph/script.py
@@ -1,15 +1,15 @@
-import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-from sklearn import tree
+import pandas as pd #import csv library
+from sklearn.tree import DecisionTreeClassifier #importing machine-learning model
+from sklearn import tree #importing tree concerning machine-learning proccesses
 
 
-music_data = pd.read_csv("project2/machine_learning_graph/music.csv")
-X = music_data.drop(columns="genre")
-y = music_data["genre"]
+music_data = pd.read_csv("project2/machine_learning_graph/music.csv") #importing csv file
+X = music_data.drop(columns="genre") #data for input
+y = music_data["genre"] #data for output
 
-model = DecisionTreeClassifier()
+model = DecisionTreeClassifier() #machine-learning model
 model.fit(X, y)
-
+#creating a graph with given features
 tree.export_graphviz(model, out_file="project2/machine_learning_graph/music-recommender.dot",
                      feature_names=["age","gender"],
                      class_names=sorted(y.unique()),
